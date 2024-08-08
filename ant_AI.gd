5546874ebfb40abfaad0745db84fb1ff942f1180
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-08-08 17:43:46 +0100

    Increase Ant AI's walking area

--- ant_AI.gd
+++ ant_AI.gd
@@ -8,7 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	progress_ratio +=0.045*delta 
+	progress +=273*delta 
 	get_child(0).global_position = lerp(get_child(0).global_position, global_position, delta*10)
 	get_child(0).global_rotation = lerp(get_child(0).global_rotation, global_rotation, delta*5)
 	
